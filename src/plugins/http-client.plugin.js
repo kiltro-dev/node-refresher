f782fee86698e8850fe6ad6340d46c612fb4d38d
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-22 17:47:12 -0300

    axios

--- src/plugins/http-client.plugin.js
+++ src/plugins/http-client.plugin.js
@@ -1,8 +1,14 @@
+const axios = require('axios');
+
 const httpClientPlugin = {
   get: async (url) => {
-    const response = await fetch(url);
-    return await response.json();
+    const { data } = await axios(url);
+    return data;
   },
+  // get: async (url) => {
+  //   const response = await fetch(url);
+  //   return await response.json();
+  // },
 
   post: async (url, body) => {},
   put: async (url, body) => {},
