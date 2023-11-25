1c98ac2824519680455e43f43868c9e6480546ad
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-24 22:26:24 -0300

    test for template

---
+++ tests/jsfoundation/01-template.test.ts
@@ -0,0 +1,15 @@
+import { emailTemplate } from './../../src/jsfoundation/01-template';
+
+describe('js-foundation/01-template', () => {
+  test('emailTemplate should contain a greeting', () => {
+    expect(emailTemplate).toContain('Hi, ');
+  });
+
+  test('emailTemplate should contain {{name}} and {{orderId}}', () => {
+    expect(emailTemplate).toMatch(/{{name}}/);
+    expect(emailTemplate).toMatch(/{{orderId}}/);
+
+    expect(emailTemplate).toContain('{{orderId}}');
+    expect(emailTemplate).toContain('{{orderId}}');
+  });
+});
